2c8f0c9c6b8e3dcedbf2e4cdf3691903920aa647
scottinet <seb.cottinet@gmail.com>
2018-06-29 11:37:21 +0200

    add a context.connection object

--- test/models/requestContext.test.js
+++ test/models/requestContext.test.js
@@ -6,11 +6,16 @@ const
 
 describe('#RequestContext', () => {
   const args = {
-    connectionId: 'connectionId',
-    protocol: 'protocol',
     token: {token: 'token'},
-    user: {user: 'user'}
+    user: {user: 'user'},
+    connection: {
+      id: 'connectionId',
+      protocol: 'protocol',
+      ips: ['foo', 'bar'],
+      foo: 'bar'
+    }
   };
+
   let context;
 
   beforeEach(() => {
@@ -18,10 +23,17 @@ describe('#RequestContext', () => {
   });
 
   it('should initialize itself with provided options', () => {
-    should(context.connectionId).eql('connectionId');
-    should(context.protocol).eql('protocol');
+    should(context.connection.id).eql('connectionId');
+    should(context.connection.protocol).eql('protocol');
+    should(context.connection.ips).match(['foo', 'bar']);
+    should(context.connection.misc.foo).eql('bar');
     should(context.token).match({token: 'token'});
     should(context.user).match({user: 'user'});
+
+    // checking deprecated properties, ensuring compatibility
+    // with older versions
+    should(context.connectionId).eql('connectionId');
+    should(context.protocol).eql('protocol');
   });
 
   it('should throw if an invalid argument type is provided', () => {
@@ -40,6 +52,17 @@ describe('#RequestContext', () => {
       should(function () { new RequestContext({[k]: 132}); }).throw(`Attribute ${k} must be of type "object"`);
       should(function () { new RequestContext({[k]: true}); }).throw(`Attribute ${k} must be of type "object"`);
     });
+
+    // string arguments for the connection sub-object
+    for (const key of ['id', 'protocol']) {
+      for (const val of [{}, [], 123, true, false]) {
+        should(() => new RequestContext({connection: {[key]: val}})).throw(`Attribute connection.${key} must be of type "string"`);
+      }
+    }
+
+    // invalid IPs arrays
+    should(() => new RequestContext({connection: {ips: 'foobar'}})).throw('Attribute connection.ips must be of type "array"');
+    should(() => new RequestContext({connection: {ips: ['foo', 123, {}]}})).throw('Attribute connection.ips must contain only values of type "string"');
   });
 
   it('should serialize properly', () => {
